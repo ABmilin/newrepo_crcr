8f93147bf8be691413694c7a766d848f70183e3c
Ichijo Aoto <166525919+ABmilin@users.noreply.github.com>
2025-08-03 04:52:37 +0900

    Delete db directory

--- db/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-// db/supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
-
-// 環境変数から読み込む
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
